39411a6fe20a3abd9c0fd6adb894fcc8e9865975
rehanchughtai97 <44495391+rehanchughtai97@users.noreply.github.com>
2020-07-24 00:22:49 +0100

    Added Multiplication Functionality

--- main.py
+++ main.py
@@ -12,4 +12,11 @@ def subtraction():
     sum = int(a) - int(b)
     print("Your answer is: " , sum)
 
+def multiplication():
+    print("Multiplication Calculator")
+    a = input("Input a number: ")
+    b = input("Input another number: ")
+    sum = int(a) * int(b)
+    print("Your answer is: " , sum)
+
     addition()
