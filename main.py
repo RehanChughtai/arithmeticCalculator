e5019cd0bbb49aab4723a2c884788b089d9f0f83
rehanchughtai97 <44495391+rehanchughtai97@users.noreply.github.com>
2020-07-24 00:18:19 +0100

    Added Subtraction function

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 def addition():
+    print("Addition Calculator")
     a = input("Input a number: ")
     b = input("Input another number: ")
     sum = int(a) + int(b)
     print("Your answer is: " , sum)
 
-addition()
+def subtraction():
+    print("Subtraction Calculator")
+    a = input("Input a number: ")
+    b = input("Input another number: ")
+    sum = int(a) - int(b)
+    print("Your answer is: " , sum)
+
+    addition()
