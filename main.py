cd3362e2a310ba02f7aaba0107ee7bf2147c4c43
rehanchughtai97 <44495391+rehanchughtai97@users.noreply.github.com>
2020-07-24 13:46:13 +0100

    Created Menu and Called functions. Comments added.

--- main.py
+++ main.py
@@ -1,3 +1,51 @@
+def getMenuChoice():
+    #Prints menu options
+    def menu():       
+        print(30 * "-", "Arithmetic Calculator", 30 * "-")
+        print("1. Addition Calculator ")
+        print("2. Subtraction Calculator ")
+        print("3. Multiplication Calculator ")
+        print("4. Division Calculator ")        
+        print("5. Exit Calculator ")
+        print(83 * "-")
+
+    loop = True
+    int_choice = -1
+    # While loop is True, continue iterating until False.
+    while loop:          
+        menu()    # Displays menu
+        choice = input("Enter your choice [1-5]: ")
+
+        if choice == '1':
+            int_choice = 1
+            #Calls addition function on choice
+            addition()
+            loop = True
+        elif choice == '2':
+            int_choice = 2
+            #Calls subtraction function on choice
+            subtraction()
+            loop = True 
+        elif choice == '3':
+            int_choice = 3
+            #Calls multiplication function on choice
+            multiplication()
+            loop = True
+        elif choice == '4':
+            int_choice = 4
+            #Calls devision function on choice
+            division()
+            loop = True
+        elif choice == '5':
+            int_choice = -1
+            print("Exiting..")
+            #Loop becomes False and exits the script
+            loop = False  
+        else:
+            #Any value other than 1-5 will validate as an error message to loop back to the menu
+            input("Wrong menu selection. Enter any key to try again..")
+    return [int_choice, choice]
+
 
 #Function to add two user inputted values together
 def addition():
@@ -32,4 +80,5 @@ def division():
     sum = int(a) / int(b)
     print("Your answer is: " , "{0:.2f}".format(sum))
 
-division()
+#Call the function to activate the menu
+getMenuChoice()
