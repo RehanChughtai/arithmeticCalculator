534881a64d2dd07fe5f9ffe07138ffad8118ef15
rehanchughtai97 <44495391+rehanchughtai97@users.noreply.github.com>
2020-07-24 01:19:25 +0100

    Added Comments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+#Function to add two user inputted values together
 def addition():
     print("Addition Calculator")
     a = input("Input a number: ")
@@ -5,6 +7,7 @@ def addition():
     sum = int(a) + int(b)
     print("Your answer is: " , sum)
 
+#Function to subtract two user inputted values together
 def subtraction():
     print("Subtraction Calculator")
     a = input("Input a number: ")
@@ -12,6 +15,7 @@ def subtraction():
     sum = int(a) - int(b)
     print("Your answer is: " , sum)
 
+#Function to multiply two user inputted values together
 def multiplication():
     print("Multiplication Calculator")
     a = input("Input a number: ")
@@ -19,6 +23,8 @@ def multiplication():
     sum = int(a) * int(b)
     print("Your answer is: " , sum)
 
+#Function to divide two user inputted values together
+#and format to 2 decimal places
 def division():
     print("Division Calculator")
     a = input("Input a number: ")
